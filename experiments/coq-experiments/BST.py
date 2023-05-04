fb51c9dee9364e902e8358f284b143a1d3e3d699
alpaylan <alpkeles99@gmail.com>
2023-05-04 11:46:11 -0400

    update experiments

--- experiments/coq-experiments/BST.py
+++ experiments/coq-experiments/BST.py
@@ -2,48 +2,38 @@ import json
 import os
 
 from benchtool.Coq import Coq
-from benchtool.Plot import all_results, create_df, generate_dashboard
 from benchtool.Types import TrialConfig, ReplaceLevel, LogLevel
 import itertools
 
-RESULTS = f'{os.getcwd()}/results/Coq/fuzzexp/BST/'
+RESULTS = f'{os.getcwd()}/results/Coq/BST/'
 
-tool = Coq(results=RESULTS, log_level=LogLevel.INFO, replace_level=ReplaceLevel.SKIP)
-do_bench, do_plot = tool.parse_args()
+tool = Coq(results=RESULTS, log_level=LogLevel.DEBUG, replace_level=ReplaceLevel.REPLACE)
 
+bench = next(bench for bench in tool.all_benches() if bench.name == 'BinarySearchTree')
+tool._preprocess(bench)
 
-if do_bench:
-    os.system("ulimit -s 65300")
-    bench = next(bench for bench in tool.all_benches() if bench.name == 'BinarySearchTree')
-    tool._preprocess(bench)
+variants = tool.all_variants(bench)
+variants.remove(next(variant for variant in variants if variant.name == 'base'))
+properties = tool.all_properties(bench)
+methods = list(method for method in tool.all_methods(bench) if method.name == 'TypeBasedFuzzer')
 
-    variants = tool.all_variants(bench)
-    variants.remove(next(variant for variant in variants if variant.name == 'base'))
-    properties = tool.all_properties(bench)
-    methods = list(method for method in tool.all_methods(bench) if method.name == 'TypeBasedFuzzer')
+cfg = json.load(open(f'experiments/coq-experiments/BinarySearchTree_exp_cfg.json'))
+tasks = cfg['tasks']
+finished = set(os.listdir(RESULTS))
+all_tasks = set(map(lambda vpm: f"BinarySearchTree,{vpm[2].name},{vpm[0].name},test_{vpm[1]}.json" if f"test_{vpm[1]}" in tasks[vpm[0].name] else None, itertools.product(variants, properties, methods)))
+all_tasks.remove(None)
+remaining_tasks = all_tasks - finished
+remaining_variants = list(set(map(lambda e: e.split(".json")[0].split(",")[2], remaining_tasks)))
 
-    cfg = json.load(open(f'experiments/coq-experiments/BST_exp_cfg.json'))
-    tasks = cfg['tasks']
-    finished = set(os.listdir(RESULTS))
-    all_tasks = set(map(lambda vpm: f"BinarySearchTree,{vpm[2].name},{vpm[0].name},test_{vpm[1]}.json" if f"test_{vpm[1]}" in tasks[vpm[0].name] else None, itertools.product(variants, properties, methods)))
-    all_tasks.remove(None)
-    remaining_tasks = all_tasks - finished
-    remaining_variants = list(set(map(lambda e: e.split(".json")[0].split(",")[2], remaining_tasks)))
-    
-    for variant in (variant for variant in tool.all_variants(bench) if ((variant.name in tasks.keys()) and (variant.name in remaining_variants))):
-        run_trial = tool.apply_variant(bench, variant, no_base=True)
-        for property in (property for property in tool.all_properties(bench) if "test_" + property in tasks[variant.name]):
-            print(f'Running {variant.name} {property}...')
-            for method in (method for method in tool.all_methods(bench) if method.name == 'TypeBasedFuzzer'):
-                cfg = TrialConfig(bench=bench,
-                                method=method.name,
-                                label=method.name,
-                                property="test_" + property,
-                                trials=10,
-                                timeout=40)
-                run_trial(cfg)
-
-if do_plot:
-    df = create_df(all_results(RESULTS))
-    generate_dashboard(df).run_server(debug=False)
-
+for variant in (variant for variant in tool.all_variants(bench) if ((variant.name in tasks.keys()) and (variant.name in remaining_variants))):
+    run_trial = tool.apply_variant(bench, variant, no_base=True)
+    for property in (property for property in tool.all_properties(bench) if "test_" + property in tasks[variant.name]):
+        print(f'Running {variant.name} {property}...')
+        for method in (method for method in tool.all_methods(bench) if method.name == 'TypeBasedFuzzer'):
+            cfg = TrialConfig(bench=bench,
+                            method=method.name,
+                            label=method.name,
+                            property="test_" + property,
+                            trials=10,
+                            timeout=40)
+            run_trial(cfg)
--- experiments/coq-experiments/IFC.py
+++ experiments/coq-experiments/IFC.py
@@ -1,7 +1,6 @@
 import os
 
 from benchtool.Coq import Coq
-from benchtool.Plot import all_results, create_df, generate_dashboard
 from benchtool.Types import TrialConfig, ReplaceLevel, LogLevel
 
 import itertools
@@ -18,28 +17,22 @@ variants = tool.all_variants(bench)
 properties = ["test_propSSNI_smart"]
 methods = (method for method in tool.all_methods(bench) if method.name == "VariationalFuzzer")
 
-
 finished = set(os.listdir(RESULTS))
 all_tasks = set(map(lambda vpm: f"IFC,{vpm[2].name},{vpm[0].name},{vpm[1]}.json", itertools.product(variants, properties, methods)))
 remaining_tasks = all_tasks - finished
 remaining_variants = list(set(map(lambda e: e.split(".json")[0].split(",")[2], remaining_tasks)))
 
-if do_bench:
-    bench = next(bench for bench in tool.all_benches() if bench.name == 'IFC')
-    tool._preprocess(bench)
-    variant = next((variant for variant in tool.all_variants(bench) if (variant.name in remaining_variants) and (variant.name != "base")))
-    run_trial = tool.apply_variant(bench, variant, no_base=True)
-    property = "propSSNI_smart"
-    method = next(method for method in tool.all_methods(bench) if method.name == "VariationalFuzzer")
-    cfg = TrialConfig(bench=bench,
-                    method=method.name,
-                    label=method.name,
-                    property="test_" + property,
-                    trials=10,
-                    timeout=60)
-    run_trial(cfg)
-
-# if do_plot:
-#     df = create_df(all_results(RESULTS))
-#     generate_dashboard(df).run_server(debug=False)
+bench = next(bench for bench in tool.all_benches() if bench.name == 'IFC')
+tool._preprocess(bench)
+variant = next((variant for variant in tool.all_variants(bench) if (variant.name in remaining_variants) and (variant.name != "base")))
+run_trial = tool.apply_variant(bench, variant, no_base=True)
+property = "propSSNI_smart"
+method = next(method for method in tool.all_methods(bench) if method.name == "VariationalFuzzer")
+cfg = TrialConfig(bench=bench,
+                method=method.name,
+                label=method.name,
+                property="test_" + property,
+                trials=10,
+                timeout=60)
+run_trial(cfg)
 
