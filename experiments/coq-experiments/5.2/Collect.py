000a9f82afdeb28421a023c72590c6d650b7ad4c
Jessica Shi <jessica.shi.4955@gmail.com>
2023-05-31 16:36:44 -0400

    fix 5.2 script

--- experiments/coq-experiments/5.2/Collect.py
+++ experiments/coq-experiments/5.2/Collect.py
@@ -6,10 +6,7 @@ from benchtool.Coq import Coq
 from benchtool.Types import TrialConfig, ReplaceLevel
 
 
-def collect(results: str, optimize: bool = True):
-    if optimize == False:
-        print('The full experiment and the scaled-down experiment are the same for this one.')
-
+def collect(results: str):
     tool = Coq(results=results, replace_level=ReplaceLevel.SKIP)
 
     for workload in tool.all_workloads():
@@ -21,22 +18,18 @@ def collect(results: str, optimize: bool = True):
         tasks_json = json.load(open(f'experiments/coq-experiments/5.1/{workload.name}_tasks.json'))
 
         for variant in tool.all_variants(workload):
-
             if variant.name == 'base':
                 continue
 
             run_trial = None
 
             for strategy in tool.all_strategies(workload):
-                if strategy.name not in [
-                    'TypeBasedFuzzer',    
-                ]:
+                if strategy.name != 'TypeBasedFuzzer':
                     continue
 
                 for property in tool.all_properties(workload):
                     property = 'test_' + property
                     if tasks_json['tasks'] and property not in tasks_json['tasks'][variant.name]:
-                        print("SKIPPING", variant.name, property)
                         continue
 
                     # Don't compile tasks that are already completed.
@@ -52,17 +45,15 @@ def collect(results: str, optimize: bool = True):
                                       strategy=strategy.name,
                                       property=property,
                                       trials=10,
-                                      timeout=60)
+                                      timeout=60,
+                                      short_circuit=True)
                     run_trial(cfg)
 
 
 if __name__ == '__main__':
     p = argparse.ArgumentParser()
     p.add_argument('--data', help='path to folder for JSON data')
-    p.add_argument('--full',
-                   help='flag to disable faster version of experiment',
-                   action='store_false')
     args = p.parse_args()
 
     results_path = f'{os.getcwd()}/{args.data}'
-    collect(results_path, args.full)
+    collect(results_path)
