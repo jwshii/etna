2975813b7b555d13bebc1f435bfa64c40c755066
liukatkat <katrinaliutinnam@gmail.com>
2023-07-31 12:55:38 -0400

    added lu parser workload

---
+++ workloads/Haskell/LuParser/src/Level.hs
@@ -0,0 +1,17 @@
+module Level where
+
+import LuSyntax
+
+level :: Bop -> Int
+level Times = 7
+level Divide = 7
+{-! -}
+level Plus = 5
+level Minus = 5
+{-!! level_1 -}
+{-!
+level Plus = 5
+level Minus = 5
+-}
+level Concat = 4
+level _ = 3 -- comparison operators
