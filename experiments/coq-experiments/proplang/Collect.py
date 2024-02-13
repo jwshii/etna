315f1069f0f18524cb3fa756855d92e53edd9177
alpaylan <alpkeles99@gmail.com>
2024-02-13 15:57:09 -0500

    create experimentation conventions

--- experiments/coq-experiments/proplang/Collect.py
+++ experiments/coq-experiments/proplang/Collect.py
@@ -15,64 +15,58 @@ def collect(results: str):
 
         tool._preprocess(workload)
 
-        variables = tool.all_variables(workload)
-        mixtures = tool.all_variable_mixtures(variables)
+        for variant in tool.all_variants(workload):
 
-        for mixture in mixtures:
-            print("Mixture")
-            for variable, version  in mixture:
-                print(f"Applying Variable {variable}::{version}")
-                tool.update_variable(workload, variable, version)
+            if variant.name == 'base':
+                continue
 
-            for variant in tool.all_variants(workload):
+            run_trial = None
 
-                if variant.name == 'base':
+            for strategy in tool.all_strategies(workload):
+                if not strategy.name.endswith('Fuzzer'):
                     continue
 
-                run_trial = None
-
-                for strategy in tool.all_strategies(workload):
-                    if not strategy.name.endswith('Fuzzer'):
+                for property in tool.all_properties(workload):
+                    if workload.name.startswith('STLC'):
+                        property = 'test_' + property + '_runner'
+                        pass
+                    elif workload.name.endswith('Proplang'):
+                        property = 'test_' + property + '_runner'
+                        if property[10:-7] not in tasks[workload.name[:3]][variant.name]:
+                            print(f'Skipping {workload.name},{strategy.name},{variant.name},{property}')
+                            continue
+                    elif workload.name == 'BST':
+                        property = 'test_' + property
+                        if property[10:] not in tasks["BST"][variant.name]:
+                            print(f'Skipping {workload.name},{strategy.name},{variant.name},{property}')
+                            continue
+                    
+                    # Don't compile tasks that are already completed.
+                    finished = set(os.listdir(results))
+                    file = f'{workload.name},{strategy.name},{variable.current},{variant.name},{property}'
+                    if f'{file}.json' in finished:
                         continue
 
-                    for property in tool.all_properties(workload):
-                        if workload.name.startswith('STLC'):
-                            property = 'test_' + property + '_runner'
-                            pass
-                        elif workload.name.endswith('Proplang'):
-                            property = 'test_' + property + '_runner'
-                            if property[10:-7] not in tasks[workload.name[:3]][variant.name]:
-                                print(f'Skipping {workload.name},{strategy.name},{variant.name},{property}')
-                                continue
-                        elif workload.name == 'BST':
-                            property = 'test_' + property
-                            if property[10:] not in tasks["BST"][variant.name]:
-                                print(f'Skipping {workload.name},{strategy.name},{variant.name},{property}')
-                                continue
-                        
-                        # Don't compile tasks that are already completed.
-                        finished = set(os.listdir(results))
-                        file = f'{workload.name},{strategy.name},{variable.current},{variant.name},{property}'
-                        if f'{file}.json' in finished:
-                            continue
+                    if not run_trial:
+                        run_trial = tool.apply_variant(workload, variant, no_base=True)
 
-                        if not run_trial:
-                            run_trial = tool.apply_variant(workload, variant, no_base=True)
-
-                        cfg = TrialConfig(workload=workload,
-                                            strategy=strategy.name,
-                                            property=property,
-                                            file=file,
-                                            trials=10,
-                                            timeout=60,
-                                            short_circuit=True)
-                        run_trial(cfg)
+                    cfg = TrialConfig(workload=workload,
+                                        strategy=strategy.name,
+                                        property=property,
+                                        file=file,
+                                        trials=10,
+                                        timeout=60,
+                                        short_circuit=True)
+                    run_trial(cfg)
 
 
 if __name__ == '__main__':
     p = argparse.ArgumentParser()
     p.add_argument('--data', help='path to folder for JSON data')
     args = p.parse_args()
+
+    if not args.data:
+        raise ValueError('No data path provided')
 
     results_path = f'{os.getcwd()}/{args.data}'
     collect(results_path)
